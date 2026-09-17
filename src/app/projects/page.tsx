182e41991667f0d4f2aedabc7bdfb9bd5290be08
gods_type <ricksue99@gmail.com>
2026-09-17 14:24:59 +0800

    lost import and i fgt to say test img for aspect ratio test

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Globe, Github } from "lucide-react";
+import Image from "next/image";
 
 const projects = [
   {
